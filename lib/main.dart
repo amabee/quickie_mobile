99be70aeb1a4c1d8057103e44382cbd679dae383
Paul Sho <Cyberkiller.CK@gmail.com>
2024-09-22 17:45:39 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
